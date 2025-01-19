523637e0a19dc0e0e6c94cb0f272e5aaefa12150
IceliosPY <82683910+IceliosPY@users.noreply.github.com>
2025-01-19 23:23:35 +0100

    Add files via upload

--- Projet_PHP/Connexion.php
+++ Projet_PHP/Connexion.php
@@ -37,20 +37,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <!-- Inclusion du fichier CSS -->
+    <link rel="stylesheet" href="./CSS/connexion.css">
 </head>
 <body>
-    <h1>Connexion</h1>
-    <?php if (isset($erreur)): ?>
-        <p style="color:red;"><?= htmlspecialchars($erreur) ?></p>
-    <?php endif; ?>
-    <form method="POST" action="">
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Se connecter</button>
-    </form>
+    <div class="form-container">
+        <h1>Connexion</h1>
+        <?php if (isset($erreur)): ?>
+            <p style="color:red;"><?= htmlspecialchars($erreur) ?></p>
+        <?php endif; ?>
+        <form method="POST" action="">
+            <label for="username">Nom d'utilisateur :</label>
+            <input type="text" id="username" name="username" required>
+            <br>
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <button type="submit">Se connecter</button>
+        </form>
+    </div>
 </body>
 </html>
